52378d909a18f48c0fa727b641ba438b7970fd81
Chinmayi <ChinmayiGanesha2007@gmail.com>
2026-06-03 19:06:12 +0000

    fix: remove build_test directory from repository

--- build_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/Amplitron
-# Build directory: /workspaces/Amplitron/build_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
